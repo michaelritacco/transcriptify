daba4045d964b4838816b13ac4d63b7267058bb6
Michael Ritacco <mdritaccons@gmail.com>
2024-06-04 13:45:48 -0400

    Adding modularity

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import pandas as pd
 import numpy as np
 import os
 
-
 st.title('YouTubeGPT')
 st.write('Paste a YouTube URL below')
 
--- youtubegpt.py
+++ youtubegpt.py
@@ -13,6 +13,10 @@ nltk.download('punkt')
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
+client = OpenAI(
+        api_key=OPENAI_API_KEY,
+    )
+
 # def load_LLM(openai_api_key):
 #     llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key, model_name='gpt-4o')
 #     return llm
@@ -24,10 +28,6 @@ def get_youtube_transcript(url):
     return transcript
 
 def process_transcript(transcription):
-    client = OpenAI(
-        api_key=OPENAI_API_KEY,
-    )
-    
     # Split the transcription by token limit
     segments = split_text_by_token_limit(transcription, max_tokens=500)
 
@@ -40,6 +40,7 @@ def process_transcript(transcription):
     return formatted_transcription
 
 def process_segment(segment):
+    global client
     response = client.chat.completions.create(
         model="gpt-4-turbo",
         messages=[
