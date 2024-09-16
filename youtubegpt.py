b75d8fbce93e904dc4e979d5101fd8c1ad9144ab
Michael Ritacco <mdritaccons@gmail.com>
2024-09-16 15:26:10 -0400

    Reverting back

--- youtubegpt.py
+++ youtubegpt.py
@@ -1,4 +1,4 @@
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import YoutubeLoader
 from openai import OpenAI
